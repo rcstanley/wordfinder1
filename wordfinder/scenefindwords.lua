75f1176ad465be46874eb709457dcb5c3b43abe9
rcstanley <rachel_c_lee@hotmail.com>
2017-03-06 14:09:11 -0700

    checks to see if words there
    checks to see if a word is good and turns a circle red or green.  Also
    got the letters to line up correctly

--- wordfinder/scenefindwords.lua
+++ wordfinder/scenefindwords.lua
@@ -18,8 +18,16 @@ local numLetters
 local useLowercase = true
 local tileSize = 40
 local fontSize1 = 40
+local isAWordCircle
 
 local words = {}
+
+--calculate where each tile/letter should go based on its location in the "word"
+local function placeTile(tileInfo)
+	tileInfo.tile.x= ((tileSize)* (tileInfo.place-1) ) + (10*tileInfo.place)
+	tileInfo.tile.y=0
+end
+
 --create a table representing each letter the user has typed in
 local function createLetterTable(sceneGroup)
 	listOfLetters = {}
@@ -32,17 +40,16 @@ local function createLetterTable(sceneGroup)
 	numLetters = letters:len()
 	--sceneGroup.anchorX=0
 	--local sceneGroup = self.view
-	--for each letter the user submitted, create an image
+
+	--for each letter the user submitted, create an image of a tile and a letter
 	for i=1,numLetters do
 		letter = string.sub(letters,i,i)
 		local letterInfo={}
 		letterInfo.letter=letter
 		letterInfo.place = i
 		displayGroup = display.newGroup()
-		displayGroup.x = (tileSize+10) * i
-		displayGroup.y = 0
-		--displayGroup.anchorX = 0
-		--display.newRect( displayGroup,0, 0, tileSize, tileSize )
+		--calculate tile position
+		
 		tileRec=display.newImageRect( displayGroup, tileImageURL, tileSize, tileSize )
 		tileRec.anchorX = 0
 		tileRec.x=0
@@ -53,6 +60,7 @@ local function createLetterTable(sceneGroup)
 		tileText.x=tileSize/2
 		tileText.y=0
 		letterInfo.tile = displayGroup
+		placeTile(letterInfo)
 		sceneGroup:insert(displayGroup)
 		letterInfo.locked = false
 		listOfLetters[i] = letterInfo 
@@ -86,6 +94,16 @@ local function loadWords()
 	file = nil
 end
 
+-- check and see if the current tile configuration is a word
+-- light up the check as green if it is
+-- set the check to red if it isn't
+local function checkTilesForWords()
+	if words[letters]==true then
+		isAWordCircle:setFillColor(0,.5,0)
+	else 
+		isAWordCircle:setFillColor(.5,0,0)
+	end
+end
 --local function place
 
 
@@ -99,22 +117,41 @@ function scene:create( event )
 	local sceneGroup = self.view
 	-- Code here runs when the scene is first created but has not yet appeared on screen
 	--put in the background
+	--load word dictionary
+	loadWords()
+	
 	local background = display.newImageRect( sceneGroup, backgroundImageURL, display.actualContentWidth, display.actualContentHeight )
 	background.x = display.contentCenterX
 	background.y = display.contentCenterY
+
+	sceneGroup.anchorX=0
+	sceneGroup.anchorY=0
 	--draw the screenshot image
 	local screenshot = composer.gameImageKey
 	sceneGroup:insert(screenshot)
+
+
+	
 	--draw the background for the tilebar
 	local displayGroup = display.newGroup()
-	local rec = display.newRect(displayGroup,0,0,display.actualContentWidth-50,50)--display.contentCenterX+10,display.actualContentHeight-60,display.actualContentWidth-50,50)
-	sceneGroup:insert(displayGroup)
-	displayGroup.x=display.contentCenterX+10
+	displayGroup.anchorX=0
+	displayGroup.anchorY=0
+	displayGroup.x=0--display.contentCenterX+10
 	displayGroup.y=display.actualContentHeight-60
+	--displayGroup.anchorChildren = true
+	local rec = display.newRect(displayGroup,0,0,display.actualContentWidth-50,50)--display.contentCenterX+10,display.actualContentHeight-60,display.actualContentWidth-50,50)
+	rec.anchorX=0
 	--get the string of letters the user entered
 	createLetterTable(displayGroup)
-	--load word dictionary
-	loadWords()
+	sceneGroup:insert(displayGroup)
+
+	local letterTitle = display.newText( sceneGroup, letters, display.contentCenterX+15, 75, native.systemFont, fontSize1-10 )
+	letterTitle.anchorX=0
+	isAWordCircle=display.newCircle(sceneGroup,display.contentCenterX,75, 15)
+	checkTilesForWords()
+	isAWordCircle:addEventListener("tap",checkTilesForWords)
+	
+	
 end
 
 
@@ -122,11 +159,7 @@ end
 local function shuffle()
 	end
 
--- check and see if the current tile configuration is a word
--- light up the check as green if it is
--- set the check to red if it isn't
-local function checkTilesForWords()
-	end
+
 
 
 -- show()
